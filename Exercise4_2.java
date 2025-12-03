09cad7bbb29d53871b5f18447632af0b942a55f7
nevayildiz <250541034@firat.edu.tr>
2025-12-03 20:50:14 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -2,22 +2,33 @@ public class Exercise4_2 {
 
   // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
 
-    public static void main(String[] args) {
-        zippo("rattle", 13);
+    public static void main(String[] args) {               //1
+        zippo("rattle", 13);                               //2
     }
 
     public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
-    }
+        System.out.println(blimp);                         //5
+        zippo("ping", -5);                                 //6
+    } 
 
     public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
+        if (flag < 0) {                                    //7
+            System.out.println(quince + " zoop");          //8
         } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+            System.out.println("ik");                      //3
+            baffle(quince);                                //4
+            System.out.println("boo-wa-ha-ha");            //9
         }
     }
 }
+
+
+
+//baffle ilk çağırıldığında blimp parametresi "rattle" dir.
+
+//çıktı : 
+ik
+rattle
+ping zoop
+boo -wa-ha-ha
+
